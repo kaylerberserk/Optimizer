e91f5e4b801789d0a0dfea1d4f0eeff64a4013d3
kaylerberserk <yohancharpentier28@gmail.com>
2026-02-21 00:16:29 +0100

    fix: Deleted `HideRecommendedSection` registry modification to prevent Windows 11 Start Menu pinned app reset bug.

--- All in One.cmd
+++ All in One.cmd
@@ -268,7 +268,6 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v St
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Start_ShowRecent /t REG_DWORD /d 0 /f >nul 2>&1
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Start_Suggestions /t REG_DWORD /d 0 /f >nul 2>&1
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowRecommendations /t REG_DWORD /d 0 /f >nul 2>&1
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v HideRecommendedSection /t REG_DWORD /d 1 /f >nul 2>&1
 echo %COLOR_GREEN%[OK]%COLOR_RESET% Interface Windows ultra-optimisee
 
 :: Desactiver la compression des papiers peints (100 = qualite maximale sans compression CPU)
